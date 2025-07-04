9da77d8c45e6133481de065d5d5a77b8a99296c9
viviss22 <130816144+viviss22@users.noreply.github.com>
2025-07-03 19:09:17 -0600

    Update csPromedio.java
    actualizacion

--- csPromedio.java
+++ csPromedio.java
@@ -6,16 +6,16 @@ public class csPromedio {
     public float nota1, nota2, nota3,nota4;
     
     public csPromedio(float n1,float n2,float n3, float n4){
-        this.nota1 = n1;
-        this.nota2 = n2;
-        this.nota3 = n3;
-        this.nota4 = n4;        
+        this.notas1 = n1;
+        this.notas2 = n2;
+        this.notas3 = n3;
+        this.notas5 = n4;        
     }
     
     public boolean calcularAprobado(){
         float promedio = 0;
         boolean vf;
-        promedio = (nota1 + nota2 + nota3 +nota4)/4;
+        promedio = (notas1 + notas2 + notas3 +notas4)/4;
         if (promedio > 61){
             vf= true;
         }else{
